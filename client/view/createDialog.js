f99d7bed46b9137620ef6928fe81b5fedbcbefdf
Ollie Edwards <olliee@mobbu.com>
2013-10-12 01:42:43 +0100

    Partial work for extended game config, need to hook ui up properly

--- client/view/createDialog.js
+++ client/view/createDialog.js
@@ -1,23 +1,60 @@
 Template.createDialog.events({
     'click .save': function(event, template) {
-        var title = template.find(".title").value;
-        var description = template.find(".description").value;
-
-        if (title.length) {
-            Meteor.call(
-                    'createGame',
-                    {
-                        title: title,
-                        description: description
-                    }
-            );
-            Session.set("showCreateDialog", false);
-        } else {
-            Session.set("createError",
-                    "It needs a title and a description, or why bother?");
-        }
+        saveGame(template);
     },
     'click .cancel': function() {
         Session.set("showCreateDialog", false);
     }
 });
+
+Template.createDialog.helpers({
+     selectionMethod : function() {
+         return _.collect(SELECTION_METHODS, function(v, k) {
+             return {
+                 method : k,
+                 label : v.label,
+                 description : v.description,
+                 checked : v.checked
+             };
+         });
+     },
+     countRaces : function() {
+        return [2,3,4,5,];
+     },
+     maxPlayers : function() {
+        return [3,4,5,6,7,8];
+     }
+});
+
+var saveGame = function(template) {
+    var game = new Game({
+        title : template.find(".title").value,
+        description : template.find(".description").value,
+        date : template.find(".date").value,
+        location : template.find(".location").value,
+        config : {
+            selectionMethod : {
+                selectionMethod : template.find(".selectionMethod").value,
+                options : {
+                    noRaces : template.find(".noRaces").value
+                }
+            },
+            maxPlayers : template.find(".maxPlayers").value,
+            racePool : generateRacePool(template)
+        }
+    });
+    
+    
+    //TODO validate
+    if (true) {
+        Meteor.call('createGame',game);
+        Session.set("showCreateDialog", false);
+    } else {
+        Session.set("createError",
+                "It needs a title and a description, or why bother?");
+    }
+}
+
+var generateRacePool = function(template) {
+    return _.pluck(Races.find({}).fetch(), "_id");
+}
