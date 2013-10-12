42d069b1965f6676ba4166e06e282f7f5178b37a
Ollie Edwards <olliee@mobbu.com>
2013-10-12 11:47:19 +0100

    Add game model and configure facebook login

---
+++ model/Game.js
@@ -0,0 +1,37 @@
+SELECTION_METHODS = {
+    PICK_FROM_SELECTION : {
+        label : "selection",
+        description : "Pick from a random selection of n races",
+        options : {
+            noRaces : 1
+        },
+        checked : 1
+    },
+    RANDOM : {
+        label : "random",
+        description : "Randomly assigmed"
+    },
+    FREE : {
+        label : 'free choice',
+        description : "Free choice of races"
+    }
+};
+
+Game = function(data) {
+    return _.extend({
+        name        : null,
+        description : null,
+        date        : null,
+        location    : null,
+        config : {
+            selectionMethod : {
+                method : PICK_FROM_SELECTION,
+                options : {
+                    noRaces : 3,
+                }
+            },
+            maxPlayers      : 6,
+            racePool        : []
+        }
+    }, (data || {}));
+};
