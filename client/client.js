c52cf5bcb9093d80746277ce389358447f1e0a97
Ollie Edwards <oliver.s.edwards@gmail.com>
2014-01-17 22:27:06 +0000

    unravel templates to fix reactivity issues

--- client/client.js
+++ client/client.js
@@ -15,31 +15,14 @@ Handlebars.registerHelper('eachMapEntries', function(context, options) {
     return ret;
 });
 
-//Handlebars.registerHelper('formatDate', function(context, options) {
-//    var date = context
-//    var day = date.getDate();
-//    if (day < 10) {
-//        day = '0' + day;
-//    }
-//    var month = date.getMonth() + 1;
-//    if (month < 10) {
-//        month = '0' + month
-//    }
-//    var hours = date.getHours();
-//    if (hours < 10) {
-//        hours = '0' + hours;
-//    }
-//    var mintues = date.getMinutes();
-//    if (mintues < 10) {
-//        mintues = '0' + mintues;
-//    }
-//    var seconds = date.getSeconds();
-//    if (seconds < 10) {
-//        seconds = '0' + seconds;
-//    }
-//    return (day + '/' + month + '/' + date.getFullYear() +
-//            ' ' + hours + ':' + mintues + ':' + seconds);
-//});
+Handlebars.registerHelper('prettyDate', function(date) {
+    if (date) {
+        return pad(date.getDate()) + "/" + pad(date.getMonth() + 1) + "/" + (date.getYear() + 1900);
+    }
+});
+var pad = function(n) {
+    return ("0"+n).slice(("" + n).length - 1);
+};
 
 Router.configure({
     layoutTemplate: 'layout'
@@ -59,6 +42,6 @@ Router.map(function() {
         action: 'create'
     });
     this.route('races', {
-       path: '/races' 
+        path: '/races'
     });
 });
