ef7a2d3b7f5f81b584d601133a6bf510ab04ef4c
Ollie Edwards <oliver.s.edwards@gmail.com>
2015-04-30 20:56:57 +0100

    read version from correct stream

--- packages/git-version/git-version.js
+++ packages/git-version/git-version.js
@@ -1,9 +1,9 @@
 version = "DEV";
 Meteor.startup(function() {
     try {
-        Npm.require("child_process").exec("git describe", function(error, sin, sout) {
+        Npm.require("child_process").exec("git describe", function(error, sout, serr) {
             if (!error) {
-                version = sout.toString();
+                version = sout;
             }
             console.log("VERSION: %s", version);
         });
