a229ff18e9b55751177cc22e4c7a605c40a6ac5a
Ollie Edwards <oliver.s.edwards@gmail.com>
2015-05-17 21:18:35 +0100

    remove user view

--- client/view/user.js
+++
@@ -1,8 +0,0 @@
-Template.user.rendered = function () {
-    $('#displayName.editable').editable({
-        success: function (response, newValue) {
-            var user = Meteor.user();
-            Meteor.users.update({_id: user._id}, {$set: {"profile.displayName": newValue}});
-        }
-    });
-};
