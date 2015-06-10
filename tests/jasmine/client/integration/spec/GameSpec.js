859a33fbbaa03af084a027b097e78d86f4a44fd3
Ollie Edwards <olliee@mobbu.com>
2015-06-10 14:23:59 +0100

    debugging client integration

--- tests/jasmine/client/integration/spec/GameSpec.js
+++ tests/jasmine/client/integration/spec/GameSpec.js
@@ -24,40 +24,12 @@ var GAME_TEMPLATE = {
     hideRaces: false
 };
 
-//todo these need to be in a utility helper somewhere
-gameHelpers = {
-    allRaces: ALL_RACES,
-    gameTemplate: GAME_TEMPLATE,
-    insertGame: insertGame
-};
-
 describe("Game", function () {
 
-    var gameId;
-    var game;
-
     describe("with races shown", function () {
-
-        beforeAll(function (done) {
-            gameId = gameHelpers.insertGame(_.extend({}, gameHelpers.gameTemplate, {
-                players: [{
-                    _id: "foo",
-                    raceSelection: ["foo", "bar", "spam"],
-                    race: null
-                }]
-            }), function (doc) {
-                game = doc;
-                done();
-            });
+        it("does stuff", function () {
+            expect(true).toBeFalsy();
         });
-
-        it("Does not provide rejected races until a user has picked", function () {
-            expect(game.rejected(game.players[0])).toBeNull();
-        });
-
     });
 
-    afterAll(function () {
-        Games.remove(gameId);
-    });
 });
